d47fc18a1f72c2b2f01a73269c1a1357de9d1e7d
Michael Hadley <michael.hadley@liferay.com>
2016-07-22 17:50:55 -0700

    Engage Turbo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+function animateBody() {
+  document.body.classList.remove('animate');
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var node = document.createElement('img');
-
-  node.addEventListener('load', function() {
-    document.body.classList.remove('animate');
-  });
-
+  node.addEventListener('load', animateBody);
   node.src = '/assets/bg.jpg';
+
+  document.addEventListener('turbolinks:render', animateBody);
 });
